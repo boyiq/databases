730312594571176617d643f9f2059573b8d4dcdc
Boyi Qu <boyiqu9596@gmail.com>
2022-09-28 16:42:02 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  roomname TEXT
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username TEXT
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  content TEXT,
+  room_id INT,
+  user_id INT,
+  FOREIGN KEY (room_id) REFERENCES rooms(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 /* Create other tables and define schemas for them here! */
+
 
 
 
