d07e976660c8c638f9028f68b3f156f9b6664d13
KateFeaster <zencornhopper@gmail.com>
2022-09-29 10:19:01 -0700

    Completed models modules

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,6 +1,28 @@
 var db = require('../db');
 
 module.exports = {
-  getAll: function () {}, // a function which produces all the messages
-  create: function () {} // a function which can be used to insert a message into the database
+  getAll: function (callback) {
+    db.connection.query('SELECT * from `messages` INNER JOIN `users` ON messages.user_id = users.id', [], (err, results)=> {
+      if (err) {
+        console.log(err);
+      } else {
+        callback(null, results);
+      }
+    })
+  }, // a function which produces all the messages
+  create: function (messageObj) {
+    db.connection.query('SELECT id from `users` where username = ?', [messageObj.username], (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        db.connection.query('INSERT INTO `messages` (content, roomname, user_id) VALUES (?, ?, ?)', [messageObj.message, messageObj.roomname, results[0].id], (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('adding a message success');
+          }
+        })
+      }
+    })
+  } // a function which can be used to insert a message into the database
 };
--- server/models/users.js
+++ server/models/users.js
@@ -2,5 +2,14 @@ var db = require('../db');
 
 module.exports = {
   getAll: function () {},
-  create: function () {}
+
+  create: function (username) {
+    db.connection.query('INSERT INTO `users` (username) VALUES (?)', [username], function(err, results, fields) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("adding a user success");
+      }
+    })
+  }
 };
